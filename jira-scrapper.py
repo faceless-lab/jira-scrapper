ce880d3d59686cfb549d6a7092d16855318dd2f2
FnV-8bY-mA3-Rxv <deimosbeta@gmail.com>
2018-04-12 02:00:45 +0300

    Removed garbage in description

--- jira-scrapper.py
+++ jira-scrapper.py
@@ -1,4 +1,5 @@
 from jira import JIRA
+import re
 
 options = {'server': 'https://jira.nuxeo.com/'}
 
@@ -6,4 +7,14 @@ jira = JIRA(options)
 
 issue = jira.issue('NXP-24421')
 
-print(issue.fields.description)
+# her = jira.search_issues('"Story Points" is not EMPTY', maxResults=100)
+
+# customfield_10492 - Story Points
+
+# print(issue.fields.customfield_10492)
+
+# print(issue.fields.project)
+
+testStr = issue.fields.description
+print(testStr)
+print(re.sub('\{.+\}', '', testStr))
